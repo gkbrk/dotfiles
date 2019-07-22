a965e415093a1b167baa7c134776fb48b5915dec
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-07-22 23:32:25 +0100

    qutebrowser: User agent

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,6 +6,11 @@
 # Uncomment this to still load settings configured via autoconfig.yml
 # config.load_autoconfig()
 
+# User agent to send. Unset to send the default. Note that the value
+# read from JavaScript is always the global value.
+# Type: String
+c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'
+
 # Enable JavaScript.
 # Type: Bool
 config.set('content.javascript.enabled', True, 'file://*')
